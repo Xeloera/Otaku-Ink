031de87df1e3d5ce4792031affeff25569add1c4
Xeloera <andrewshearer98@hotmail.com>
2023-04-09 14:28:20 +0100

    commint 2

---
+++ frontend/online-store/src/componests/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* Your navbar content goes here */}
+        </nav>
+    );
+};
+
+export default Navbar;
